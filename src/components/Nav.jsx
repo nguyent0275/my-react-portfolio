13d24223953a678e3c94eb15c50f405eca5707a3
Toan <nguyentoan0275@gmail.com>
2024-02-23 15:40:36 -0500

    replaced <a> tags in navbar with React <Link> tags

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,9 @@
+import { Link, useLocation } from "react-router-dom";
+
 // exporting the Nav component for App.jsx to use for every page
 export default function Nav() {
   const linkStyle = { border: "1px black", padding: "5px" };
+  const currentPage = useLocation().pathname;
 
   return (
     <nav className="main-header-menu">
@@ -14,16 +17,36 @@ export default function Nav() {
         }}
       >
         <div style={linkStyle}>
-          <a href="/about">About Me</a>
+          <Link
+            to="./About"
+            className={
+              currentPage === "/About" ? "nav-link active" : "nav-link"
+            }
+          >About</Link>
         </div>
         <div style={linkStyle}>
-          <a href="/portfolio">Portfolio</a>
+          <Link
+            to="./Portfolio"
+            className={
+              currentPage === "/Portfolio" ? "nav-link active" : "nav-link"
+            }
+          >Portfolio</Link>
         </div>
         <div style={linkStyle}>
-          <a href="/contact">Contact</a>
+          <Link
+            to="./Contact"
+            className={
+              currentPage === "/Contact" ? "nav-link active" : "nav-link"
+            }
+          >Contact</Link>
         </div>
         <div style={linkStyle}>
-          <a href="/resume">Resume</a>
+          <Link
+            to="./Resume"
+            className={
+              currentPage === "/Resume" ? "nav-link active" : "nav-link"
+            }
+          >Resume</Link>
         </div>
       </section>
     </nav>
